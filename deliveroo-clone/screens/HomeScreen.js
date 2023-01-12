5f2cd427a75bea31016628a69741592ec1eeab13
LaupWing <loc_strict@hotmail.com>
2023-01-12 13:19:54 +0100

    update

--- deliveroo-clone/screens/HomeScreen.js
+++ deliveroo-clone/screens/HomeScreen.js
@@ -5,6 +5,7 @@ import { Image, SafeAreaView, StatusBar, Text, View } from "react-native"
 
 const StyledText = styled(Text)
 const StyledImage = styled(Image)
+const StyledView = styled(View)
 
 const HomeScreen = () => {
    const navigation = useNavigation()
@@ -17,17 +18,22 @@ const HomeScreen = () => {
 
    return (
       <SafeAreaView>
-         <View>
+         <StyledView className="flex-row pb-3 items-center mx-4 space-x-2">
             <StyledImage
                source={{
                   uri: "https://links.papareact.com/wru"
                }}
-               className="h-7 w-7 bg-gray-300"
+               className="h-7 w-7 bg-gray-300 rounded-full"
             />
-         </View>
-         <StyledText className="text-red-600">
-            
-         </StyledText>
+            <View>
+               <StyledText className="font-bold text-gray-400 text-xs">
+                  Deliver now
+               </StyledText>
+               <StyledText className="font-bold text-xl">
+                  Deliver now
+               </StyledText>
+            </View>
+         </StyledView>
       </SafeAreaView>
    )
 }
