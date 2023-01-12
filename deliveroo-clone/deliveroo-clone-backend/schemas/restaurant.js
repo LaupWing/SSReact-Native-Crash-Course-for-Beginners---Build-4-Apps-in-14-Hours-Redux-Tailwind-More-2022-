de29c0443c2273ebbd6d3e12966a30c447113a78
LaupWing <loc_strict@hotmail.com>
2023-01-12 14:54:29 +0100

    udpate

--- deliveroo-clone/deliveroo-clone-backend/schemas/restaurant.js
+++ deliveroo-clone/deliveroo-clone-backend/schemas/restaurant.js
@@ -6,9 +6,36 @@ export default defineType({
    type: 'document',
    fields: [
       defineField({
-         name: 'title',
-         title: 'Title',
-         type: 'string',
+         name: "name",
+         title: "Restaurant Name",
+         type: "string",
+         validation: (Rule) => Rule.required()
+      }),
+      defineField({
+         name: "short_description",
+         title: "Short Description",
+         type: "string",
+         validation: (Rule) => Rule.max(200)
+      }),
+      defineField({
+         name: "image",
+         title: "Image of restaurant",
+         type: "image"
+      }),
+      defineField({
+         name: "lat",
+         title: "Latitude of the restaurant",
+         type: "number",
+      }),
+      defineField({
+         name: "long",
+         title: "Longitude of the restaurant",
+         type: "number"
+      }),
+      defineField({
+         name: "address",
+         title: "Restaurant address",
+         type: "string"
       }),
    ],
 })
