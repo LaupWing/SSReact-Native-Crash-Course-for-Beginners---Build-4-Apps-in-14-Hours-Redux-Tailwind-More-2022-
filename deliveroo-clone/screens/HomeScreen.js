4b1c7381417ab4cb1f5ef6244bcfc57e69ea7136
LaupWing <loc_strict@hotmail.com>
2023-01-12 13:36:34 +0100

    minor

--- deliveroo-clone/screens/HomeScreen.js
+++ deliveroo-clone/screens/HomeScreen.js
@@ -25,7 +25,7 @@ const HomeScreen = () => {
 
    return (
       <StyledSafeAreaView className="bg-white pt-5">
-         <StyledView className="flex-row pb-3 items-center mx-4 space-x-2">
+         <StyledView className="flex-row pb-3 items-center mx-4 space-x-2 px-2">
             <StyledImage
                source={{
                   uri: "https://links.papareact.com/wru"
@@ -45,7 +45,7 @@ const HomeScreen = () => {
 
          </StyledView>
          <StyledView className="flex-row items-center space-x-2 pb-2 mx-4">
-            <StyledView className="flex-row space-x-2 flex-1 bg-gray-200 p-3">
+            <StyledView className="flex-row space-x-2 flex-1 bg-gray-200 p-3 px-4">
                <MagnifyingGlassIcon color={"gray"} size={20}/>
                <TextInput 
                   placeholder="Restaurants and cuisines"
