3cf0975007554eacd05b6483c924706ebe91007f
LaupWing <loc_strict@hotmail.com>
2023-01-12 14:30:21 +0100

    rating

--- deliveroo-clone/components/RestaurantCard.js
+++ deliveroo-clone/components/RestaurantCard.js
@@ -1,9 +1,12 @@
 import { styled } from "nativewind"
 import { View, Text, TouchableOpacity, Image } from "react-native"
+import { StarIcon } from "react-native-heroicons/solid"
+import { MapPinIcon } from "react-native-heroicons/outline"
 
 const StyledTouchableOpacity = styled(TouchableOpacity)
 const StyledImage = styled(Image)
 const StyledText = styled(Text)
+const StyledView = styled(View)
 
 const RestaurantCard = ({
    id,
@@ -25,9 +28,15 @@ const RestaurantCard = ({
             }}
             className="w-64 h-36 rounded-sm"
          />
-         <View>
+         <StyledView className="px-3 pb-4">
             <StyledText className="font-bold text-lg pt-2">{title}</StyledText>
-         </View>
+            <StyledView className="flex-row items-center space-x-1">
+               <StarIcon color={"green"} opacity={0.5} size={22}/>
+               <StyledText className="text-gray-500">
+                  <StyledText className="text-green-500">{rating}</StyledText> . {genre}
+               </StyledText>
+            </StyledView>
+         </StyledView>
       </StyledTouchableOpacity>
    )
 }
