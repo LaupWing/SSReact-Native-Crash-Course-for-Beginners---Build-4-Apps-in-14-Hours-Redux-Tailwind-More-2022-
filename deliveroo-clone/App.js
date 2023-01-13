364b5a33dc4de32e32dc6c25edef630e39e5dc28
LaupWing <loc_strict@hotmail.com>
2023-01-13 11:57:07 +0100

    Restuarnt screen

--- deliveroo-clone/App.js
+++ deliveroo-clone/App.js
@@ -3,6 +3,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack"
 import { styled, withExpoSnack } from "nativewind"
 import { StyleSheet, Text, View, StatusBar } from "react-native"
 import HomeScreen from "./screens/HomeScreen"
+import RestaurantScreen from "./screens/RestaurantScreen"
 
 
 const Stack = createNativeStackNavigator()
@@ -13,6 +14,7 @@ const App = () => {
          <StatusBar/>
          <Stack.Navigator>
             <Stack.Screen name="Home" component={HomeScreen}/>
+            <Stack.Screen name="Restaurant" component={RestaurantScreen}/>
          </Stack.Navigator>
       </NavigationContainer>
    )
