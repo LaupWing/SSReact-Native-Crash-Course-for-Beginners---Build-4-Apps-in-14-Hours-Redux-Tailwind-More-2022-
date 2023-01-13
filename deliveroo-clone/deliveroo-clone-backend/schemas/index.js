ac0d458bb1c2e5abd90e286c6e16557016fb92eb
LaupWing <loc_strict@hotmail.com>
2023-01-13 10:58:23 +0100

    added

--- deliveroo-clone/deliveroo-clone-backend/schemas/index.js
+++ deliveroo-clone/deliveroo-clone-backend/schemas/index.js
@@ -1,9 +1,11 @@
 import category from "./category"
 import restaurant from "./restaurant"
 import dish from './dish'
+import featured from './featured'
 
 export const schemaTypes = [
    restaurant, 
    dish, 
+   featured, 
    category
 ]
