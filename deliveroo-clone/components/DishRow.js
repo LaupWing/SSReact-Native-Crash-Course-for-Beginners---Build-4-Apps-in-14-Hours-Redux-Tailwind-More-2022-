401ad64200f59f186147006d625bf98e4c3ff55d
LaupWing <loc_strict@hotmail.com>
2023-01-13 12:22:18 +0100

    ufspyr

--- deliveroo-clone/components/DishRow.js
+++ deliveroo-clone/components/DishRow.js
@@ -1,5 +1,10 @@
-import { View, Text } from "react-native"
+import { View, Text, TouchableOpacity } from "react-native"
 import React from "react"
+import { styled } from "nativewind"
+
+const StyledTouchableOpacity = styled(TouchableOpacity)
+const StyledView = styled(View)
+const StyledText = styled(Text)
 
 const DishRow = (
    id,
@@ -9,9 +14,12 @@ const DishRow = (
    image,
 ) => {
    return (
-      <View>
-         <Text>DishRow</Text>
-      </View>
+      <StyledTouchableOpacity>
+         <StyledView>
+            <StyledText className="text-lg mb-1">{name}</StyledText>
+            <StyledText className="text-gray-400">{description}</StyledText>
+         </StyledView>
+      </StyledTouchableOpacity>
    )
 }
 
