314d15cbf20c9db6e6a89aa760c373cab7cd122d
LaupWing <loc_strict@hotmail.com>
2023-01-13 10:52:51 +0100

    Dish

--- deliveroo-clone/deliveroo-clone-backend/schemas/index.js
+++ deliveroo-clone/deliveroo-clone-backend/schemas/index.js
@@ -1,6 +1,9 @@
-import blockContent from './blockContent'
-import category from './category'
-import restaurant from './restaurant'
-import author from './author'
+import category from "./category"
+import restaurant from "./restaurant"
+import dish from './dish'
 
-export const schemaTypes = [restaurant, author, category, blockContent]
+export const schemaTypes = [
+   restaurant, 
+   dish, 
+   category
+]
