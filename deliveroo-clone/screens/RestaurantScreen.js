f111f8e269a719737d71282b5bcbc165265c63d9
LaupWing <loc_strict@hotmail.com>
2023-01-13 12:19:40 +0100

    Dishrow

--- deliveroo-clone/screens/RestaurantScreen.js
+++ deliveroo-clone/screens/RestaurantScreen.js
@@ -12,6 +12,7 @@ import {
 import {
    QuestionMarkCircleIcon
 } from "react-native-heroicons/outline"
+import DishRow from "../components/DishRow"
 
 const StyledScrollView = styled(ScrollView)
 const StyledView = styled(View)
@@ -89,9 +90,29 @@ const RestaurantScreen = () => {
                </StyledText>
             </StyledView>
 
-            <StyledTouchableOpacity>
+            <StyledTouchableOpacity className="flex-row items-center space-x-2 p-4 border-y border-gray-300">
                <QuestionMarkCircleIcon color={"gray"} opacity={0.6} size={20}/>
+               <StyledText className="pl-2 flex-1 text-md font-bold">
+                  Have a food allergy?
+               </StyledText>
+               <ChevronRightIcon color={"#00CCBB"}/>
             </StyledTouchableOpacity>
+         </StyledView>
+
+         <StyledView>
+            <StyledText className="px-4 pt-6 mb-3 font-bold text-xl">
+               Menu
+            </StyledText>
+            {dishes.map(dish => (
+               <DishRow
+                  key={dish._id}
+                  id={dish._id}
+                  name={dish.name}
+                  description={dish.short_description}
+                  price={dish.price}
+                  image={dish.image}
+               />
+            ))}
          </StyledView>
       </StyledScrollView>
    )
