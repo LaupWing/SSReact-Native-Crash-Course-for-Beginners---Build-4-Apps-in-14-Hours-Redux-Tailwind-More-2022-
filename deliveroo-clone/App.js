2b2f503c07a5e727429483699c1d0a521fb5aa8e
LaupWing <loc_strict@hotmail.com>
2023-01-16 10:42:14 +0100

    react redux

--- deliveroo-clone/App.js
+++ deliveroo-clone/App.js
@@ -2,8 +2,10 @@ import { NavigationContainer } from "@react-navigation/native"
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
 import { styled, withExpoSnack } from "nativewind"
 import { StyleSheet, Text, View, StatusBar } from "react-native"
+import { Provider } from "react-redux"
 import HomeScreen from "./screens/HomeScreen"
 import RestaurantScreen from "./screens/RestaurantScreen"
+import { store } from "./store"
 
 
 const Stack = createNativeStackNavigator()
@@ -11,11 +13,13 @@ const Stack = createNativeStackNavigator()
 const App = () => {
    return (
       <NavigationContainer>
-         <StatusBar/>
-         <Stack.Navigator>
-            <Stack.Screen name="Home" component={HomeScreen}/>
-            <Stack.Screen name="Restaurant" component={RestaurantScreen}/>
-         </Stack.Navigator>
+         <Provider store={store}>
+            <StatusBar/>
+            <Stack.Navigator>
+               <Stack.Screen name="Home" component={HomeScreen}/>
+               <Stack.Screen name="Restaurant" component={RestaurantScreen}/>
+            </Stack.Navigator>
+         </Provider>
       </NavigationContainer>
    )
 }
