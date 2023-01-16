5a1adbac18d2a0697d0b8ffcd66b75549ef2fa51
LaupWing <loc_strict@hotmail.com>
2023-01-16 10:36:29 +0100

    udpate

--- deliveroo-clone/components/DishRow.js
+++ deliveroo-clone/components/DishRow.js
@@ -3,6 +3,7 @@ import React from "react"
 import { styled } from "nativewind"
 import Currency from "react-currency-formatter"
 import { urlFor } from "../sanity"
+import { MinusCircleIcon, PlusCircleIcon } from  "react-native-heroicons/solid"
 
 const StyledTouchableOpacity = styled(TouchableOpacity)
 const StyledView = styled(View)
@@ -16,24 +17,50 @@ const DishRow = (
    price,
    image,
 ) => {
+   const [isPressed, setIsPressed] = useState(false)
+
+
    return (
-      <StyledTouchableOpacity>
-         <StyledView>
-            <StyledText className="text-lg mb-1">{name}</StyledText>
-            <StyledText className="text-gray-400">{description}</StyledText>
-            <StyledText className="text-gray-400 mt-2">
-               <Currency currency="GBP" quantity={price}/>
-            </StyledText>
-         </StyledView>
-         <StyledView>
-            <StyledImage
-               source={{
-                 uri: urlFor(image).url()
-               }}
-               className="h-20 w-20 bg-gray-300 p-4"
-            />
-         </StyledView>
-      </StyledTouchableOpacity>
+      <>
+         <StyledTouchableOpacity 
+            onPress={() => setIsPressed(!isPressed)} 
+            className="bg-white border p-4 border-gray-200"
+         >
+            <StyledView className="flex-row">
+               <StyledView className="flex-1 pr-2">
+                  <StyledText className="text-lg mb-1">{name}</StyledText>
+                  <StyledText className="text-gray-400">{description}</StyledText>
+                  <StyledText className="text-gray-400 mt-2">
+                     <Currency currency="GBP" quantity={price}/>
+                  </StyledText>
+               </StyledView>
+               <StyledView>
+                  <StyledImage
+                     style={{
+                        borderWidth: 1,
+                        borderColor: "#F3F3F4"
+                     }}
+                     source={{
+                     uri: urlFor(image).url()
+                     }}
+                     className="h-20 w-20 bg-gray-300 p-4"
+                  />
+               </StyledView>
+            </StyledView>
+         </StyledTouchableOpacity>
+         {isPressed && (
+            <StyledView>
+               <StyledView>
+                  <StyledTouchableOpacity>
+                     <MinusCircleIcon
+                        // color={items.length > 0 ? "#00CCBB" : "gray"}
+                        size={40}
+                     />
+                  </StyledTouchableOpacity>
+               </StyledView>
+            </StyledView>
+         )}
+      </>
    )
 }
 
