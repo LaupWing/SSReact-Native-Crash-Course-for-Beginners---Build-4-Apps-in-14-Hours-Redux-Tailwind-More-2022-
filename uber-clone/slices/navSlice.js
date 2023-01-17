7963ea9e5fb125fa92fceeb49ee49ddebf0bab8b
LaupWing <loc_strict@hotmail.com>
2023-01-17 12:05:08 +0100

    store connect

--- uber-clone/slices/navSlice.js
+++ uber-clone/slices/navSlice.js
@@ -28,8 +28,8 @@ export const {
    setTravelTimeInformation
 } = navSlice.actions
 
-export const selectOrigin = state => state.origin 
-export const selectDestination = state => state.destination 
-export const selectTravelTimeInformation = state => state.travelTimeInformation 
+export const selectOrigin = state => state.nav.origin 
+export const selectDestination = state => state.nav.destination 
+export const selectTravelTimeInformation = state => state.nav.travelTimeInformation 
 
 export default navSlice.reducer
--- uber-clone/store.js
+++ uber-clone/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit"
+import navReducer from "./slices/navSlice"
 
 export const store = configureStore({
    reducer: {
-      nav: null,
+      nav: navReducer,
    },
 })
