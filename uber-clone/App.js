5bcf8057da65cf2e719ec771699e4e65a140779a
LaupWing <loc_strict@hotmail.com>
2023-01-17 12:12:36 +0100

    udpate

--- uber-clone/App.js
+++ uber-clone/App.js
@@ -1,15 +1,13 @@
 import { StatusBar } from "expo-status-bar"
 import { StyleSheet, Text, View } from "react-native"
 import { Provider } from "react-redux"
+import Homescreen from "./screens/Homescreen"
 import { store } from "./store"
 
 export default function App() {
    return (
       <Provider store={store}>
-         <View style={styles.container}>
-            <Text>Open up App.js to start working on your app!</Text>
-            <StatusBar style="auto" />
-         </View>
+         <Homescreen/>
       </Provider>
    )
 }
