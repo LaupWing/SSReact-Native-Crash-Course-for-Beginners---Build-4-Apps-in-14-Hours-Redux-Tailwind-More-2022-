cda9fccf2aff35d6c573af979e211c8e40a7fc6d
LaupWing <loc_strict@hotmail.com>
2023-01-18 10:39:59 +0100

    update

--- uber-clone/screens/Homescreen.js
+++ uber-clone/screens/Homescreen.js
@@ -1,10 +1,18 @@
-import { View, Text, SafeAreaView } from "react-native"
+import { View, Text, SafeAreaView, Image } from "react-native"
 import React from "react"
 
 const Homescreen = () => {
    return (
-      <SafeAreaView>
-         <Text className="text-red-400">Homescreen</Text>
+      <SafeAreaView className="bg-white h-full">
+         <View className="p-5">
+            <Image
+               source={{
+                  uri: "https://links.papareact/gzs"
+               }}
+               className="w-full h-full object-contain"
+            />
+         </View>
+         <Text className="text-red-400 p-10">Homescreen</Text>
       </SafeAreaView>
    )
 }
