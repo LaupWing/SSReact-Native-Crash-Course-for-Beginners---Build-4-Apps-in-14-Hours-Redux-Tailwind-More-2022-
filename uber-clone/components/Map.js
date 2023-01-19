4eb07ad6b1888a3bc7931bd4ec63baab11131fe4
LaupWing <loc_strict@hotmail.com>
2023-01-19 11:18:14 +0100

    update

--- uber-clone/components/Map.js
+++ uber-clone/components/Map.js
@@ -1,10 +1,12 @@
 import React from "react"
 import MapView, { Marker } from "react-native-maps"
+import MapViewDirections from "react-native-maps-directions"
 import { useSelector } from "react-redux"
-import { selectOrigin } from "../slices/navSlice"
+import { selectDestination, selectOrigin } from "../slices/navSlice"
 
 const Map = () => {
    const origin = useSelector(selectOrigin)
+   const destination = useSelector(selectDestination)
 
    return (
       <MapView
@@ -17,6 +19,15 @@ const Map = () => {
             longitudeDelta: 0.005
          }}
       >
+         {origin && destination && (
+            <MapViewDirections
+               origin={origin.description}
+               destination={destination.description}
+               apikey=""
+               strokeWidth={3}
+               strokeColor="black"
+            />
+         )}
          {origin?.location && (
             <Marker 
                coordinate={{
--- uber-clone/components/NavigateCard.js
+++ uber-clone/components/NavigateCard.js
@@ -1,7 +1,13 @@
+import { useNavigation } from "@react-navigation/native"
 import { Text, SafeAreaView } from "react-native"
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete"
+import { useDispatch } from "react-redux"
+import { setDestination } from "../slices/navSlice"
 
 const NavigateCard = () => {
+   const dispatch = useDispatch()
+   const navigation = useNavigation()
+
    return (
       <SafeAreaView className="bg-white flex-1">
          <Text className="text-center py-5 text-xl">Good Morning, Sonny</Text>
@@ -10,7 +16,20 @@ const NavigateCard = () => {
                <GooglePlacesAutocomplete
                   placeholder="Where to?"
                   debounce={400}
+                  onPress={(data, details = null) => {
+                     dispatch(setDestination({
+                        location: details.geometry.location,
+                        description: data.description
+                     }))
+                     navigation.navigate("RideOptionsCard")
+                  }}
                   nearbyPlacesAPI="GooglePlacesSearch"
+                  className="bg-white pt-5 flex-0"
+                  enablePoweredByContainer={false}
+                  query={{
+                     key: "",
+                     language: "en"
+                  }}
                />
             </View>
          </View>
