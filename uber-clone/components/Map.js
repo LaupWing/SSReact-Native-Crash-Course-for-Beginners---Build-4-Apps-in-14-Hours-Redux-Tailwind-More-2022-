780b232dda5da5eed9a1e3fd684830582855d193
LaupWing <loc_strict@hotmail.com>
2023-01-19 10:58:34 +0100

    udpate

--- uber-clone/components/Map.js
+++ uber-clone/components/Map.js
@@ -1,18 +1,35 @@
 import { View, Text } from "react-native"
 import React from "react"
-import MapView from "react-native-maps"
+import MapView, { Marker } from "react-native-maps"
+import { useSelector } from "react-redux"
+import { selectOrigin } from "../slices/navSlice"
 
 const Map = () => {
+   const origin = useSelector(selectOrigin)
+
    return (
       <MapView
          className="flex-1"
+         mapType="mutedStandard"
          initialRegion={{
-            latitude: 1,
-            longitude: 1,
-            latitudeDelta: 1,
-            longitudeDelta: 1
+            latitude: origin.location.lat,
+            longitude: origin.location.long,
+            latitudeDelta: 0.005,
+            longitudeDelta: 0.005
          }}
-      />
+      >
+         {origin?.location && (
+            <Marker 
+               coordinate={{
+                  latitude: origin.location.lat,
+                  longitude: origin.location.long,
+               }}
+               title="Origin"
+               description={origin.description}
+               identifier="origin"
+            />
+         )}
+      </MapView>
    )
 }
 
