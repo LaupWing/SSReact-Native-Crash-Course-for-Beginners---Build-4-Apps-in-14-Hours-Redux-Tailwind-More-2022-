5636d46a5f29d1ab36d3d20578733c26c7b0d10b
LaupWing <loc_strict@hotmail.com>
2023-01-19 11:44:06 +0100

    keyboard

--- uber-clone/App.js
+++ uber-clone/App.js
@@ -1,5 +1,6 @@
 import { NavigationContainer } from "@react-navigation/native"
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import { KeyboardAvoidingView, Platform } from "react-native"
 import { SafeAreaProvider } from "react-native-safe-area-context"
 import { Provider } from "react-redux"
 import Homescreen from "./screens/Homescreen"
@@ -14,24 +15,29 @@ export default function App() {
       <Provider store={store}>
          <NavigationContainer>
             <SafeAreaProvider>
-               <Stack.Navigator>
-                  <Stack.Screen 
-                     name="HomeScreen"
-                     component={Homescreen}
-                     options={{
-                        headerShown: false
-                     }}
-                  />
-                  <Stack.Screen 
-                     name="MapScreen"
-                     component={MapScreen}
-                     options={{
-                        headerShown: false,
-                        animation: "slide_from_right"
-                     }}
-                  />
-               </Stack.Navigator>
-               {/* <Homescreen /> */}
+               <KeyboardAvoidingView 
+                  behavior={Platform.OS === "ios" ? "padding" : "height"}
+                  className="flex-1"
+                  keyboardVerticalOffset={Platform.OS === "ios" ? -64 : 0}
+               >
+                  <Stack.Navigator>
+                     <Stack.Screen 
+                        name="HomeScreen"
+                        component={Homescreen}
+                        options={{
+                           headerShown: false
+                        }}
+                     />
+                     <Stack.Screen 
+                        name="MapScreen"
+                        component={MapScreen}
+                        options={{
+                           headerShown: false,
+                           animation: "slide_from_right"
+                        }}
+                     />
+                  </Stack.Navigator>
+               </KeyboardAvoidingView>
             </SafeAreaProvider>
          </NavigationContainer>
       </Provider>
