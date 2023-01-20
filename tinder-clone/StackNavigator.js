f66dfcd7f7314412c748b2fa437d8fbc30d3da40
LaupWing <loc_strict@hotmail.com>
2023-01-20 11:35:40 +0100

    satck navigator

---
+++ tinder-clone/StackNavigator.js
@@ -0,0 +1,14 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import { View, Text } from "react-native"
+import HomeScreen from "./screens/HomeScreen"
+
+const Stack = createNativeStackNavigator()
+
+const StackNavigator = () => {
+   return (
+      <Stack.Navigator>
+         <Stack.Screen name="Home" component={HomeScreen}/>
+      </Stack.Navigator>
+   )
+}
+export default StackNavigator
