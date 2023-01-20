72c2ae63126ed12317d9720aa42ff6eb1c8410a4
LaupWing <loc_strict@hotmail.com>
2023-01-20 12:26:39 +0100

    update

--- tinder-clone/hooks/useAuth.js
+++ tinder-clone/hooks/useAuth.js
@@ -1,8 +1,14 @@
 import { createContext, useContext } from "react"
+import * as Google from "expo-auth-session"
 
 const AuthContext = createContext({})
 
 export const AuthProvider = ({ children }) => {
+
+   const signInWithGoogle = async () => {
+      await Google.useAuthRequest()
+   }
+   
    return (
       <AuthContext.Provider value={null}>
          {children}
