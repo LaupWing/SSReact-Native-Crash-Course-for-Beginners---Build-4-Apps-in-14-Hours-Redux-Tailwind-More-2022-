24c46227af435424331755406272ca6975af848d
LaupWing <loc_strict@hotmail.com>
2023-01-20 11:22:39 +0100

    installed more

--- tinder-clone/App.js
+++ tinder-clone/App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from "expo-status-bar"
-import { StyleSheet, Text, View } from "react-native"
+import { Button, Text, View } from "react-native"
 
 export default function App() {
    return (
       <View className="flex-1 justify-center items-center">
          <Text>Open up App.js to start working on your app!</Text>
          <StatusBar style="auto" />
+         <Button
+            title="Click me"
+         />
       </View>
    )
 }
