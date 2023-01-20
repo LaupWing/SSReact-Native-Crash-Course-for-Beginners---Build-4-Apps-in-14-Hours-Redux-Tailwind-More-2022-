b22b07b8d706627d5cf4399781b1522be5e5d2d6
LaupWing <loc_strict@hotmail.com>
2023-01-20 11:36:29 +0100

    stack natigator

--- tinder-clone/App.js
+++ tinder-clone/App.js
@@ -1,14 +1,7 @@
-import { StatusBar } from "expo-status-bar"
-import { Button, Text, View } from "react-native"
+import StackNavigator from "./StackNavigator"
 
 export default function App() {
    return (
-      <View className="flex-1 justify-center items-center">
-         <Text>Open up App.js to start working on your app!</Text>
-         <StatusBar style="auto" />
-         <Button
-            title="Click me"
-         />
-      </View>
+      <StackNavigator/>
    )
 }
