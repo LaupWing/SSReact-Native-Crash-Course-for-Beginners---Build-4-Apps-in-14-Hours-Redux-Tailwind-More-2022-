1d62224e62d4361e7166a1017525f030277c3276
LaupWing <loc_strict@hotmail.com>
2023-01-24 12:34:27 +0100

    update

--- tinder-clone/screens/MatchScreen.js
+++ tinder-clone/screens/MatchScreen.js
@@ -1,5 +1,11 @@
+import { useNavigation, useRoute } from "@react-navigation/native"
 import { View, Text } from "react-native"
+
 const MatchScreen = () => {
+   const navigation = useNavigation()
+   const { params} = useRoute()
+   const {loggedInProfile, userSwiped} = params
+
    return (
       <View>
          <Text>MatchScreen</Text>
