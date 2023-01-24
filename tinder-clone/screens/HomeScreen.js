2f831a15c70605dee9113a4404a4d9ced3720443
LaupWing <loc_strict@hotmail.com>
2023-01-24 11:08:51 +0100

    udpatre

--- tinder-clone/screens/HomeScreen.js
+++ tinder-clone/screens/HomeScreen.js
@@ -1,6 +1,6 @@
 import { useNavigation } from "@react-navigation/native"
-import { useLayoutEffect } from "react"
-import { View, Text, Button, SafeAreaView, TouchableOpacity, Image } from "react-native"
+import { useLayoutEffect, useRef } from "react"
+import { View, Text, Button, SafeAreaView, TouchableOpacity, Image, StyleSheet } from "react-native"
 import useAuth from "../hooks/useAuth"
 import { AntDesign, Entypo, Ionicons } from "@expo/vector-icons"
 import Swiper from "react-native-deck-swiper"
@@ -8,6 +8,7 @@ import Swiper from "react-native-deck-swiper"
 const HomeScreen = () => {
    const navigation = useNavigation()
    const { user, logout } = useAuth()
+   const swipeRef = useRef(null)
 
    return (
       <SafeAreaView className="flex-1">
@@ -42,6 +43,7 @@ const HomeScreen = () => {
          </View>
          <View className="flex-1 -mt-6">
             <Swiper 
+               ref={swipeRef}
                containerStyle={{
                   backgroundColor: "transparent"
 
@@ -50,7 +52,28 @@ const HomeScreen = () => {
                stackSize={5}
                cardIndex={0}
                verticalSwipe={false}
+               onSwipedLeft={() => {}}
+               onSwipedRight={() => {}}
                animateCardOpacity
+               overlayLabels={{
+                  left: {
+                     title: "NOPE",
+                     style:{
+                        label:{
+                           textAlign: "right",
+                           color: "red"
+                        }
+                     }
+                  },
+                  right: {
+                     title: "MATCH",
+                     style:{
+                        label:{
+                           color: "green"
+                        }
+                     }
+                  },
+               }}
                renderCard={card => (
                   <View 
                      className="bg-white h-3/4 rounded-xl relative"
@@ -62,17 +85,50 @@ const HomeScreen = () => {
                            uri: card.photoURL
                         }}   
                      />
-                     <View>
+                     <View 
+                        className="bg-white flex-row w-full h-20 absolute bottom-0 justify-between items-center px-6 py-2 rounded-b-xl"
+                        style={styles.cardShadow}
+                     >
                         <View>
-                           <Text>{card.firstName} {card.lastName}</Text>
-                           <Text>{card.job}</Text>
+                           <Text className="text-xl font-bold">{card.firstName} {card.lastName}</Text>
+                           <Text className="">{card.job}</Text>
                         </View>
+                        <Text className="text-2xl">{card.age}</Text>
                      </View>
                   </View>
                )}
             />
          </View>
+
+         <View className="flex flex-row justify-evenly">
+            <TouchableOpacity 
+               className="items-center justify-center rounded-full w-16 h-16 bg-red-200"
+               onPress={() => swipeRef.current.swipeLeft()}
+               >
+               <Entypo color={"red"} name="cross" size={24}/>
+            </TouchableOpacity>
+            <TouchableOpacity 
+               className="items-center justify-center rounded-full w-16 h-16 bg-green-200"
+               onPress={() => swipeRef.current.swipeRight()}
+            >
+               <AntDesign color={"green"} name="heart" size={24}/>
+            </TouchableOpacity>
+         </View>
       </SafeAreaView>
    )
 }
 export default HomeScreen
+
+
+const styles = StyleSheet.create({
+   cardShadow: {
+      shadowColor: "#000",
+      shadowOffset:{
+         width: 0,
+         height: 1
+      },
+      shadowOpacity: 0.2,
+      shadowRadius: 1.41,
+      elevation: 2
+   }
+})
