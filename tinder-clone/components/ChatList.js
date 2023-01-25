3051a0252931f1349428f1c7f4fde21a91b50102
LaupWing <loc_strict@hotmail.com>
2023-01-25 10:48:43 +0100

    udpate

--- tinder-clone/components/ChatList.js
+++ tinder-clone/components/ChatList.js
@@ -1,6 +1,17 @@
+import { useEffect, useState } from "react"
 import { View, Text } from "react-native"
+import { collection, onSnapshot, query, where } from "firebase/firestore"
 
 const ChatList = () => {
+   const [matches, setMatches] = useState([])
+
+   useEffect(() => 
+      onSnapshot(query(collection(db, "matches"), where("usersMatches", "array-contains", user.uid)), snapshot => setMatches(snapshot.docs.map(doc =>({
+         ...doc.data(),
+         id: doc.id
+      }))))
+   , [])
+
    return (
       <View>
          <Text>ChatList</Text>
