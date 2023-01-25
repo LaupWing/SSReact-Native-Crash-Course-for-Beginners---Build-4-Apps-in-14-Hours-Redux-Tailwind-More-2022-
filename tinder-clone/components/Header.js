aa10e9ce7db2ccffc42b399b9304b6b77e4bebe4
LaupWing <loc_strict@hotmail.com>
2023-01-25 10:39:53 +0100

    fking hell

--- tinder-clone/components/Header.js
+++ tinder-clone/components/Header.js
@@ -1,9 +1,36 @@
-import { View, Text } from "react-native"
+import { View, Text, TouchableOpacity } from "react-native"
+import { Foundation } from "@expo/vector-icons"
+import { Ionicons } from "@expo/vector-icons"
+import { useNavigation } from "@react-navigation/native"
 
-const Header = () => {
+const Header = ({ title, callEnabled}) => {
+   const navigation = useNavigation()
+
    return (
-      <View>
-         <Text>Header</Text>
+      <View className="flex-row p-2 items-center justify-between">
+         <View className="flex flex-row items-center">
+            <TouchableOpacity
+               className="p-2"
+               onPress={() => navigation.goBack()}
+            >
+               <Ionicons 
+                  name="chevron-back-outline"
+                  size={34}
+                  color="#FF584"
+               />
+               <Text className="text-2xl font-bold pl-2">{title}</Text>
+            </TouchableOpacity>
+         </View>
+         {callEnabled && (
+            <TouchableOpacity className="rounded-full mr-4 p-3 bg-red-200">
+               <Foundation
+                  className=""
+                  name="telephone"
+                  size={20}
+                  color="red"
+               />
+            </TouchableOpacity>
+         )}
       </View>
    )
 }
