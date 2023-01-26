8a88847a37a507b1b0ddc4c359a66c85861ef579
LaupWing <loc_strict@hotmail.com>
2023-01-26 10:53:35 +0100

    update

--- signal-clone/App.js
+++ signal-clone/App.js
@@ -1,10 +1,30 @@
 import { NavigationContainer } from "@react-navigation/native"
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
 import { StatusBar } from "expo-status-bar"
 import { StyleSheet, Text, View } from "react-native"
+import LoginScreen from "./screens/LoginScreen"
+
+const Stack = createNativeStackNavigator()
+
+const globlScreenOptions = {
+   headerStyle: {
+      backgroundColor: "blue"
+   },
+   headerTitleStyle: {
+      color: "white"
+   },
+   headerTintColor: "white"
+}
 
 export default function App() {
    return (
       <NavigationContainer>
+         <Stack.Navigator screenOptions={globlScreenOptions}>
+            <Stack.Screen 
+               name="Login" 
+               component={LoginScreen}
+            />
+         </Stack.Navigator>
          <View style={styles.container}>
             <Text>Open up App.js to start working on your app!</Text>
             <StatusBar style="auto" />
