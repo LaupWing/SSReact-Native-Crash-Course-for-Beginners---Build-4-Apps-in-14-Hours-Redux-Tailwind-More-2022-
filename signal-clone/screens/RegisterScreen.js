e5a2fc6e7299d719e1e3c85cef3274d3c293c17f
LaupWing <loc_strict@hotmail.com>
2023-01-26 11:37:28 +0100

    update

--- signal-clone/screens/RegisterScreen.js
+++ signal-clone/screens/RegisterScreen.js
@@ -1,27 +1,74 @@
-import { Input } from "@rneui/base"
+import { Input, Text } from "@rneui/base"
+import { Button } from "@rneui/themed"
 import { StatusBar } from "expo-status-bar"
-import { useState } from "react"
-import { View, Text, KeyboardAvoidingView } from "react-native"
+import { useLayoutEffect, useState } from "react"
+import { View, KeyboardAvoidingView } from "react-native"
+import { auth } from "../firebase"
 
-const RegisterScreen = () => {
+const RegisterScreen = ({navigation}) => {
    const [name, setName] = useState("")
+
+   const register = () => {
+      auth.createUserWithEmailAndPassword(name, name)
+         .then(user => {
+            user.user.update({
+               displayName: name,
+               photoURL: ""
+            })
+         })
+         .catch(err => alert(err.message))
+   }
+
+   useLayoutEffect(() => {
+      navigation.setOptions({
+         headerBackTitle: "Back to login"
+      })
+   },[navigation])
 
    return (
       <KeyboardAvoidingView 
          behavior="padding"
-         className=""
+         className="flex-1 bg-white items-center justify-center p-10"
       >
          <StatusBar style="light"/>
          <Text h3 className="mb-5">
             Create a signal account
          </Text>
-         <View className="">
+         <View className="w-30">
             <Input
                placeholder="Full Name"
                autoFocus
                textContentType="name"
+               value={name}
+               onChangeText={setName}
+            />
+            <Input
+               placeholder="Full Name"
+               textContentType="name"
+            />
+            <Input
+               placeholder="Full Name"
+               textContentType="name"
+            />
+            <Input
+               placeholder="Full Name"
+               textContentType="name"
+            />
+            <Input
+               placeholder="Full Name"
+               textContentType="name"
             />
          </View>
+         <Button
+            title={"Register"}
+            onPress={register}
+            raised
+            containerStyle={{
+               width: 200,
+               marginTop: 10
+            }}
+         />
+         <View className="h-20"/>
       </KeyboardAvoidingView>
    )
 }
