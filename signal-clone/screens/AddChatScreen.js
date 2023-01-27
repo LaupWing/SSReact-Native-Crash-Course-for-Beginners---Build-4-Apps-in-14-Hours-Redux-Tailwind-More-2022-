21243748e80a7dfdbf8dff4da12977755efd87d3
LaupWing <loc_strict@hotmail.com>
2023-01-27 10:41:54 +0100

    udpate

--- signal-clone/screens/AddChatScreen.js
+++ signal-clone/screens/AddChatScreen.js
@@ -1,9 +1,39 @@
+import { Button, Icon, Input } from "@rneui/base"
+import { useLayoutEffect, useState } from "react"
 import { View, Text } from "react-native"
+import { db } from "../firebase"
 
 const AddChatScreen = ({ navigation }) => {
+   const [input, setInput] = useState("")
+
+   useLayoutEffect(() => {
+      navigation.setOptions({
+         title: "Add a new Chat",
+         headerBackTitle: "Chats",
+
+      })
+   }, [navigation])
+
+   const createChat = async () => {
+      await db.collection("chats").add({
+         chatName: input
+      }).then(() => navigation.goBack())
+      .catch(err => alert(err.message))
+   }
+
    return (
-      <View>
-         <Text>AddChatScreen</Text>
+      <View className="bg-white p-10 h-full">
+         <Input
+            placeholder="Enter a new chat"
+            value={input}
+            onChangeText={setInput}
+            onSubmitEditing={createChat}
+            leftIcon={<Icon name="wechat" type="antdesign" color={"black"}/>}
+         />
+         <Button
+            onPress={createChat}
+            title="Create a new Chat"
+         />
       </View>
    )
 }
